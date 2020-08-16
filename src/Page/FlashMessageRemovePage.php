6162ba231180448e1ec76aa1480ca29a88bf975c
Paul Water <p.r.water@setbased.nl>
2020-08-16 16:51:39 +0200

    Add page for removing a flash message.

---
+++ src/Page/FlashMessageRemovePage.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\FlashMessage\Page;
+
+use Plaisio\C;
+use Plaisio\Exception\BadRequestException;
+use Plaisio\Kernel\Nub;
+use Plaisio\Page\CorePage;
+use Plaisio\Response\NullResponse;
+use Plaisio\Response\Response;
+
+/**
+ * Page for removing a flash message.
+ */
+class FlashMessageRemovePage extends CorePage
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the URL of this page.
+   *
+   * @return string
+   */
+  public static function getUrl(): string
+  {
+    return Nub::$nub->cgi->putId('pag', C::PAG_ID_PLAISIO_FLASH_MESSAGE_REMOVE, 'pag');
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritDoc
+   */
+  public function checkAuthorization(): void
+  {
+    if (!Nub::$nub->request->isPost())
+    {
+      throw new BadRequestException('POST expected');
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @inheritDoc
+   */
+  public function handleRequest(): Response
+  {
+    Nub::$nub->flashMessage->removeFlashMessage($_POST['id']);
+
+    return $this->response = new NullResponse();
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
